dec094b8b618bb7fbe37fbabf7afa3ca3d8f98d1
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-06-29 09:17:02 +0100

    FEAT: Function returns 'Fizz' for all muliples of 3

--- katas/fizz-buzz/fizz-buzz.js
+++ katas/fizz-buzz/fizz-buzz.js
@@ -1,9 +1,12 @@
 /**
  * Result generator for Fizz Buzz game
  * @param {number} number - Current number
- * @returns {string} The number given as a string
+ * @returns {string} The number given as a string Or Fizz is the number is a multiple of 3
  */
 function fizzBuzz(number) {
+  if (Number.isInteger(number/3)){
+    return "Fizz";
+  }
   return number.toString();
 }
 
--- katas/fizz-buzz/fizz-buzz.test.js
+++ katas/fizz-buzz/fizz-buzz.test.js
@@ -7,3 +7,11 @@ test.each([
 ])('fizzBuzz converts standard numbers to their string equivalent', (input, expected) => {
   expect(fizzBuzz(input)).toBe(expected);
 });
+
+test.each([
+  [3, "Fizz"],
+  [6, "Fizz"],
+  [9, "Fizz"]
+])('fizzBuzz returns Fizz for integers of 3', (input, expected) => {
+  expect(fizzBuzz(input)).toBe(expected);
+});
